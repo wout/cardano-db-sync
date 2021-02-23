60d03c70e726b8a48c4b97a2c6767f61d0399490
Artur Wieczorek <artur.wieczorek@iohk.io>
2021-02-23 23:16:09 +0100

    Fix scripts 1

--- sync-tests/start_db_sync.sh
+++ sync-tests/start_db_sync.sh
@@ -42,31 +42,31 @@ shift $((OPTIND-1))
 
 function get_network_param_value() {
 
-	if [ "$environment" = "mainnet"  ] 
+	if [ "$environment" = "mainnet"  ]
 	    then
 	        echo "--mainnet"
-	
+
 	elif [ "$environment" = "testnet" ]
 		then
 	    	echo "--testnet-magic 1097911063"
-	
+
 	elif [ "$environment" = "staging" ]
-		then    
+		then
 	    	echo "--testnet-magic 633343913"
-	
+
 	elif [ "$environment" = "shelley_qa" ]
 		then
 	    	echo "--testnet-magic 3"
-	
+
 		else:
-	    	echo "" 
+	    	echo ""
 		fi
 }
 
 function get_block_from_tip() {
 
 	 ./../cardano-node/cardano-cli query tip $(get_network_param_value) |
-	 
+
 	while read -r line
 	do
 		if [[ $line == *"block"* ]]; then
@@ -81,7 +81,7 @@ function get_latest_db_synced_slot() {
 	local log_filepath=$1
 	IN=$(tail -n 1 $log_filepath)
 	preformated_string=$(echo "$IN" | sed 's/^.*slot/slot/') # this will return: "slot 19999, block 20000, hash 683be7324c47df71e2a234639a26d7747f1501addbba778636e66f3a18a46db7"
-	
+
 	IFS=' ' read -ra ADDR <<< "$preformated_string"
 	for i in "${!ADDR[@]}"; do
 		if [[ "${ADDR[$i]}" == *"slot"* ]]; then # we get the index $i for slot keyword - we know that slot number has ${i+1) position
@@ -93,33 +93,33 @@ function get_latest_db_synced_slot() {
 
 function calculate_latest_node_slot_for_environment() {
 
-	if [ "$environment" = "mainnet"  ] 
+	if [ "$environment" = "mainnet"  ]
 	then
-	        byron_start_time_in_seconds=1506203091   # 2017-09-23 21:44:51 
+	        byron_start_time_in_seconds=1506203091   # 2017-09-23 21:44:51
 	        shelley_start_time_in_seconds=1596059091 # 2020-07-29 21:44:51
-	        allegra_start_time_in_seconds=1608155091 # 2020-12-16 21:44:51 
-	        
+	        allegra_start_time_in_seconds=1608155091 # 2020-12-16 21:44:51
+
 	elif [ "$environment" = "testnet" ]
 	then
 	        byron_start_time_in_seconds=1563999616    # 2019-07-24 20:20:16
 	        shelley_start_time_in_seconds=1595967616  # 2020-07-28 20:20:16
 	        allegra_start_time_in_seconds=1608063616  # 2020-12-15 20:20:16
-	
+
 	elif [ "$environment" = "staging" ]
-	then    
+	then
 	        byron_start_time_in_seconds=1506450213    # 2017-09-26 18:23:33
 	        shelley_start_time_in_seconds=1596306213  # 2020-08-01 18:23:33
 	        allegra_start_time_in_seconds=1608402213  # 2020-12-19 18:23:33
-	
+
 	elif [ "$environment" = "shelley_qa" ]
 	then
 	        byron_start_time_in_seconds=1597669200    # 2020-08-17 13:00:00
 	        shelley_start_time_in_seconds=1597683600  # 2020-08-17 17:00:00
 	        allegra_start_time_in_seconds=1607367600  # 2020-12-07 19:00:00
 	else:
-	    echo "" 
+	    echo ""
 	fi
-	
+
 	slots_in_epoch=432000
 	current_time=$(date +'%s')
 	current_slot=$(( (shelley_start_time_in_seconds - byron_start_time_in_seconds)/20  + current_time - shelley_start_time_in_seconds - slots_in_epoch/3))
@@ -132,7 +132,7 @@ network_param=$(get_network_param_value)
 
 ######################################## GET CONFIGS FOR ALL NETWORKS ########################################
 
-cd ../cardano-db-sync
+cd ..
 cd config
 
 echo ""
@@ -171,7 +171,7 @@ PGPASSFILE=config/pgpass-${environment} cabal run cardano-db-sync-extended -- \
 --config config/${environment}-config.yaml \
 --socket-path ../cardano-node/${environment}/node.socket \
 --state-dir ledger-state/${environment} \
---schema-dir schema/ >> $db_sync_logfile & 
+--schema-dir schema/ >> $db_sync_logfile &
 
 sleep 60
 
@@ -185,7 +185,7 @@ echo "AFTER: db_sync_logfile"
 
 re='^[0-9]+$'
 while ! [[ $latest_db_synced_slot =~ $re ]] ; do
-   echo "Not a slot number, waiting for proper log line that contains slot number..." 
+   echo "Not a slot number, waiting for proper log line that contains slot number..."
    sleep 20
    latest_db_synced_slot=$(get_latest_db_synced_slot $db_sync_logfile)
 done
