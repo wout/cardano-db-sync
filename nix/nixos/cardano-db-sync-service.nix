b5fff2b0ac71614111c2a0616f44da7a79cab341
Michael Fellinger <michael.fellinger@iohk.io>
2021-10-01 14:46:35 +0200

    use stateDir for snapshot restore

--- nix/nixos/cardano-db-sync-service.nix
+++ nix/nixos/cardano-db-sync-service.nix
@@ -4,7 +4,8 @@ let
   cfg = config.services.cardano-db-sync;
   self = cfg.dbSyncPkgs;
   envConfig = cfg.environment;
-  configFile = __toFile "db-sync-config.json" (__toJSON (cfg.explorerConfig // cfg.logConfig));
+  configFile = __toFile "db-sync-config.json"
+    (__toJSON (cfg.explorerConfig // cfg.logConfig));
   stateDirBase = "/var/lib/";
   majorVersion = lib.versions.major cfg.package.version;
 in {
@@ -32,7 +33,7 @@ in {
           Restore a snapshot before starting cardano-db-sync,
           if the snasphot file given by the option exist.
           Snapshot file is deleted after restore.
-          '';
+        '';
       };
       restoreSnapshotSha = lib.mkOption {
         type = lib.types.nullOr lib.types.str;
@@ -53,36 +54,47 @@ in {
         type = lib.types.str;
         internal = true;
         default = lib.optionalString (cfg.restoreSnapshot != null) ''
-        ${lib.optionalString (lib.hasPrefix "$" cfg.restoreSnapshot) ''if [ ! -z "''${${lib.removePrefix "$" cfg.restoreSnapshot}:-}" ]; then''}
-        SNAPSHOT_BASENAME="$(basename "${cfg.restoreSnapshot}")"
-        RESTORED_MARKER="$SNAPSHOT_BASENAME.restored"
-        if [ ! -f "$RESTORED_MARKER" ]; then
-          if [[ "${cfg.restoreSnapshot}" =~ ^https://.* ]]; then
-            echo "Downloading snapshot ${cfg.restoreSnapshot} ..."
-            ${pkgs.curl}/bin/curl -LOC - "${cfg.restoreSnapshot}"
+          ${lib.optionalString (lib.hasPrefix "$" cfg.restoreSnapshot) ''
+            if [ ! -z "''${${
+              lib.removePrefix "$" cfg.restoreSnapshot
+            }:-}" ]; then''}
+          SNAPSHOT_BASENAME="$(basename "${cfg.restoreSnapshot}")"
+          RESTORED_MARKER="${cfg.stateDir}/$SNAPSHOT_BASENAME.restored"
+          if [ ! -f "$RESTORED_MARKER" ]; then
+            if [[ "${cfg.restoreSnapshot}" =~ ^https://.* ]]; then
+              echo "Downloading snapshot ${cfg.restoreSnapshot} ..."
+              ${pkgs.curl}/bin/curl -LOC - "${cfg.restoreSnapshot}"
 
-            ${if (cfg.restoreSnapshotSha != null)
-            then  ''echo "${cfg.restoreSnapshotSha} $SNAPSHOT_BASENAME" > "$SNAPSHOT_BASENAME.sha256sum"''
-            else  ''${pkgs.curl}/bin/curl -LO "${cfg.restoreSnapshot}.sha256sum"''
-            }
-            ${pkgs.coreutils}/bin/sha256sum -c "$SNAPSHOT_BASENAME.sha256sum"
+              ${
+                if (cfg.restoreSnapshotSha != null) then
+                  ''
+                    echo "${cfg.restoreSnapshotSha} $SNAPSHOT_BASENAME" > "$SNAPSHOT_BASENAME.sha256sum"''
+                else
+                  ''
+                    ${pkgs.curl}/bin/curl -LO "${cfg.restoreSnapshot}.sha256sum"''
+              }
+              ${pkgs.coreutils}/bin/sha256sum -c "$SNAPSHOT_BASENAME.sha256sum"
 
-            ${lib.optionalString (cfg.restoreSnapshotSigKey != null) ''
-              ${pkgs.curl}/bin/curl -LO "${cfg.restoreSnapshot}.asc"
-              ${pkgs.gnupg}/bin/gpg --keyserver keys.openpgp.org --recv-keys ${cfg.restoreSnapshotSigKey}
-              ${pkgs.gnupg}/bin/gpg --verify "$SNAPSHOT_BASENAME.asc" "$SNAPSHOT_BASENAME"
-            ''}
+              ${
+                lib.optionalString (cfg.restoreSnapshotSigKey != null) ''
+                  ${pkgs.curl}/bin/curl -LO "${cfg.restoreSnapshot}.asc"
+                  ${pkgs.gnupg}/bin/gpg --keyserver keys.openpgp.org --recv-keys ${cfg.restoreSnapshotSigKey}
+                  ${pkgs.gnupg}/bin/gpg --verify "$SNAPSHOT_BASENAME.asc" "$SNAPSHOT_BASENAME"
+                ''
+              }
 
-            SNAPSHOT="$SNAPSHOT_BASENAME"
-          else
-            SNAPSHOT="${cfg.restoreSnapshot}"
+              SNAPSHOT="$SNAPSHOT_BASENAME"
+            else
+              SNAPSHOT="${cfg.restoreSnapshot}"
+            fi
+            rm -f ${cfg.stateDir}/*.lstate
+            ${
+              ../../scripts/postgresql-setup.sh
+            } --restore-snapshot "$SNAPSHOT" ${cfg.stateDir}
+            touch $RESTORED_MARKER
+            rm -f $SNAPSHOT{,.sha256sum,.asc}
           fi
-          rm -f ${cfg.stateDir}/*.lstate
-          ${../../scripts/postgresql-setup.sh} --restore-snapshot "$SNAPSHOT" ${cfg.stateDir}
-          touch $RESTORED_MARKER
-          rm -f $SNAPSHOT{,.sha256sum,.asc}
-        fi
-        ${lib.optionalString (lib.hasPrefix "$" cfg.restoreSnapshot) "fi"}
+          ${lib.optionalString (lib.hasPrefix "$" cfg.restoreSnapshot) "fi"}
         '';
       };
       # FIXME: is my assumption that this is required correct?
@@ -116,7 +128,7 @@ in {
       };
       dbSyncPkgs = lib.mkOption {
         type = lib.types.attrs;
-        default = import ../. {};
+        default = import ../. { };
         defaultText = "cardano-db-sync pkgs";
         description = ''
           The cardano-db-sync packages and library that should be used.
@@ -126,7 +138,10 @@ in {
       };
       package = lib.mkOption {
         type = lib.types.package;
-        default = if cfg.extended then self.cardano-db-sync-extended else self.cardano-db-sync;
+        default = if cfg.extended then
+          self.cardano-db-sync-extended
+        else
+          self.cardano-db-sync;
       };
       user = lib.mkOption {
         type = lib.types.str;
@@ -165,44 +180,55 @@ in {
   };
   config = lib.mkIf cfg.enable {
     services.cardano-db-sync = let
-      exec = if cfg.extended then "${cfg.package}/bin/cardano-db-sync-extended" else "${cfg.package}/bin/cardano-db-sync";
+      exec = if cfg.extended then
+        "${cfg.package}/bin/cardano-db-sync-extended"
+      else
+        "${cfg.package}/bin/cardano-db-sync";
     in {
-      pgpass = builtins.toFile "pgpass" "${cfg.postgres.socketdir}:${toString cfg.postgres.port}:${cfg.postgres.database}:${cfg.postgres.user}:*";
+      pgpass = builtins.toFile "pgpass" "${cfg.postgres.socketdir}:${
+          toString cfg.postgres.port
+        }:${cfg.postgres.database}:${cfg.postgres.user}:*";
       script = pkgs.writeShellScript "cardano-db-sync" ''
         set -euo pipefail
 
-        export PATH="$PATH:${lib.makeBinPath (with pkgs; [
-          coreutils
-          self.cardanoDbSyncHaskellPackages.cardano-db-tool.components.exes.cardano-db-tool
-          postgresql_11
-          bash
-          gnutar
-          gzip
-        ])}"
+        export PATH="$PATH:${
+          lib.makeBinPath (with pkgs; [
+            coreutils
+            self.cardanoDbSyncHaskellPackages.cardano-db-tool.components.exes.cardano-db-tool
+            postgresql_11
+            bash
+            gnutar
+            gzip
+          ])
+        }"
 
-        ${if (cfg.socketPath == null) then ''if [ -z "''${CARDANO_NODE_SOCKET_PATH:-}" ]
-        then
-          echo "You must set \$CARDANO_NODE_SOCKET_PATH"
-          exit 1
-        fi'' else "export CARDANO_NODE_SOCKET_PATH=\"${cfg.socketPath}\""}
+        ${if (cfg.socketPath == null) then ''
+          if [ -z "''${CARDANO_NODE_SOCKET_PATH:-}" ]
+                  then
+                    echo "You must set \$CARDANO_NODE_SOCKET_PATH"
+                    exit 1
+                  fi'' else
+          ''export CARDANO_NODE_SOCKET_PATH="${cfg.socketPath}"''}
 
         ${lib.optionalString cfg.postgres.generatePGPASS ''
-        cp ${cfg.pgpass} ./pgpass
-        chmod 0600 ./pgpass
-        export PGPASSFILE=$(pwd)/pgpass
+          cp ${cfg.pgpass} ./pgpass
+          chmod 0600 ./pgpass
+          export PGPASSFILE=$(pwd)/pgpass
         ''}
 
         ${lib.optionalString cfg.takeSnapshot ''
-        for f in db-sync-snapshot-schema-${majorVersion}*; do
-          if [ -f $f ]; then
-            echo "Skipping snapshot: already exist for this version: $f"
-            SKIP_SNAPSHOT=1
+          for f in db-sync-snapshot-schema-${majorVersion}*; do
+            if [ -f $f ]; then
+              echo "Skipping snapshot: already exist for this version: $f"
+              SKIP_SNAPSHOT=1
+            fi
+          done
+          if [ -z ''${SKIP_SNAPSHOT:-} ]; then
+            SNAPSHOT_SCRIPT=$(cardano-db-tool prepare-snapshot --state-dir ${cfg.stateDir} | tail -n 1)
+            ${
+              ../../scripts/postgresql-setup.sh
+            } ''${SNAPSHOT_SCRIPT#*scripts/postgresql-setup.sh}
           fi
-        done
-        if [ -z ''${SKIP_SNAPSHOT:-} ]; then
-          SNAPSHOT_SCRIPT=$(cardano-db-tool prepare-snapshot --state-dir ${cfg.stateDir} | tail -n 1)
-          ${../../scripts/postgresql-setup.sh} ''${SNAPSHOT_SCRIPT#*scripts/postgresql-setup.sh}
-        fi
         ''}
 
         ${cfg.restoreSnapshotScript}
@@ -221,7 +247,7 @@ in {
         home = "/var/lib/${cfg.user}";
         group = cfg.user;
       };
-      groups.cexplorer = {};
+      groups.cexplorer = { };
     };
     systemd.services.cardano-db-sync = {
       wantedBy = [ "multi-user.target" ];
@@ -236,24 +262,25 @@ in {
       ];
       preStart = ''
         for x in {1..10}; do
-          ${pkgs.netcat}/bin/nc -z localhost ${toString cfg.postgres.port} && break
+          ${pkgs.netcat}/bin/nc -z localhost ${
+            toString cfg.postgres.port
+          } && break
           echo loop $x: waiting for postgresql 2 sec...
           sleep 2
         done
       '';
       serviceConfig = {
-        ExecStart        = cfg.script;
-        User             = cfg.postgres.user;
+        ExecStart = cfg.script;
+        User = cfg.postgres.user;
         WorkingDirectory = cfg.stateDir;
-        StateDirectory   = lib.removePrefix stateDirBase cfg.stateDir;
+        StateDirectory = lib.removePrefix stateDirBase cfg.stateDir;
       };
     };
 
-    assertions = [
-      {
-        assertion = lib.hasPrefix stateDirBase cfg.stateDir;
-        message = "The option services.cardano-db-sync.stateDir should have ${stateDirBase} as a prefix!";
-      }
-    ];
+    assertions = [{
+      assertion = lib.hasPrefix stateDirBase cfg.stateDir;
+      message =
+        "The option services.cardano-db-sync.stateDir should have ${stateDirBase} as a prefix!";
+    }];
   };
 }
